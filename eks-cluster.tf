0ce8339a1dbfe073285c7a5242802908e5b53ffc
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2022-06-08 16:04:21 +0000

    update instance

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -62,17 +62,6 @@ data "aws_iam_policy_document" "eks_dev_iam_policy_document" {
 }
 
 
-# resource "aws_iam_policy" "eks_dev_iam_policy" {
-#   name   = "eks_dev_iam_policy"
-#   policy = data.aws_iam_policy_document.eks_dev_iam_policy_document.json
-# }
-
-# resource "aws_iam_role_policy_attachment" "eks_dev_iam_policy_attachment" {
-#   role       = aws_iam_role.eks_dev_role.name
-#   policy_arn = aws_iam_policy.eks_dev_iam_policy.arn
-# }
-
-
 data "aws_iam_policy_document" "eks_dev_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
